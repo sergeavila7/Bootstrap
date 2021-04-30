b0bdf52bdb7f476415028193ea5a077b550f263c
serge_avila7 <serge_avila7@yahoo.com>
2021-04-30 16:22:44 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip()
+})
+
+const links = document.querySelectorAll(".navbar ul a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
